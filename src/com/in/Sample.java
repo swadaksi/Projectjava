9d819125ba6987a49d4f36fedeadbecb6283496a
swadaksi <sswathi1917@gmail.com>
2024-09-12 09:49:36 +0530

    message

---
+++ src/com/in/Sample.java
@@ -0,0 +1,15 @@
+package com.in;
+
+public class Sample {
+	public void m1() {
+		System.out.println("m1");
+	}
+
+	public void m2() {
+		System.out.println("m2");
+	}
+
+	public void m3() {
+		System.out.println("m3");
+	}
+}
